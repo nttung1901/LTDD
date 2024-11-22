93a62b4115f222e94bc5b78250016bcd169646a4
tung nguyen <nttung1901@gmail.com>
2024-11-23 01:49:03 +0700

    buoi2

--- Quiz/app/src/main/java/com/nttung/quiz/MainActivity.java
+++ Quiz/app/src/main/java/com/nttung/quiz/MainActivity.java
@@ -1,113 +1,141 @@
 package com.nttung.quiz;
-
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
+    private TextView mTextViewQuesTion;
     private Button mButtonTrue;
     private Button mButtonFalse;
-    private Button mButtonNext;
     private Button mButtonPrevious;
-    private TextView mTextViewQuestion;
-    private Button mButtonCheat;
-    private int mCurrentIndex = 0;
-    private boolean mIsCheater;
+    private Button mButtonNext;
 
-    private Question[] mQuestionsBank= {
-            new Question(R.string.question1,true),
-            new Question(R.string.question2,false),
-            new Question(R.string.question3,false),
-            new Question(R.string.question4,true),
-            new Question(R.string.question5,true),
+    private int mCurrentIndex = 0;
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_1, true),
+            new Question(R.string.question_2, false),
+            new Question(R.string.question_3, true),
+            new Question(R.string.question_4, false),
+            new Question(R.string.question_5, true)
     };
 
-    public void updateQuestion(){
-        mTextViewQuestion.setText(mQuestionsBank[mCurrentIndex].getmTestResId());
+    private void updateQuestion(){
+        mTextViewQuesTion.setText(mQuestionBank[mCurrentIndex].getmTextResId());
     }
 
+    private void checkAnswer(boolean userPressTrue){
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].ismAnswerTrue();
+
+        int messageId =0;
+        if(userPressTrue == answerIsTrue)
+            messageId = R.string.toast_true;
+        else
+            messageId = R.string.toast_false;
+        Toast.makeText(this, messageId, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate() called");
         setContentView(R.layout.activity_main);
 
-
-
-        mTextViewQuestion= findViewById(R.id.text_view_question);
+        mTextViewQuesTion = findViewById(R.id.textview_question);
         updateQuestion();
+
         mButtonTrue = findViewById(R.id.button_true);
         mButtonTrue.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(mQuestionsBank[mCurrentIndex].ismAnswerTrue())
-                    Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_LONG).show();
-                else
-                    Toast.makeText(MainActivity.this,R.string.incorrect_toast,Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+                checkAnswer(true);
             }
         });
 
         mButtonFalse = findViewById(R.id.button_false);
         mButtonFalse.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(!mQuestionsBank[mCurrentIndex].ismAnswerTrue())
-                    Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_LONG).show();
-                else
-                    Toast.makeText(MainActivity.this,R.string.incorrect_toast,Toast.LENGTH_LONG).show();
-            }
-        });
-
-
-        mButtonNext= findViewById(R.id.button_next);
-        mButtonNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mCurrentIndex = (mCurrentIndex + 1)% mQuestionsBank.length;
-                updateQuestion();
+            public void onClick(View v) {
+                checkAnswer(false);
             }
         });
 
         mButtonPrevious = findViewById(R.id.button_previous);
         mButtonPrevious.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(mCurrentIndex >0)
-                    mCurrentIndex = mCurrentIndex - 1;
+            public void onClick(View v) {
+                if(mCurrentIndex>0) {
+                    mCurrentIndex--;
+                    updateQuestion();
+                }
+            }
+        });
+
+        mButtonNext = findViewById(R.id.button_next);
+        mButtonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1)%mQuestionBank.length;
                 updateQuestion();
             }
         });
 
+        if(savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        }
 
-        mButtonCheat = findViewById(R.id.button_cheat);
-        mButtonCheat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                boolean answer = mQuestionsBank[mCurrentIndex].ismAnswerTrue();
-                Intent i = CheatActivity.newIntent(MainActivity.this,answer);
-                startActivity(i);
-            }
-        });
+        updateQuestion();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
 
-        ActivityResultLauncher<Intent> starActivity4Result= registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
 
-                    }
-                }
-        );
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(KEY_INDEX, "onSaveInstanceState");
+        outState.putInt(KEY_INDEX, mCurrentIndex);
     }
 }
--- Quiz/app/src/main/java/com/nttung/quiz/Question.java
+++ Quiz/app/src/main/java/com/nttung/quiz/Question.java
@@ -1,24 +1,24 @@
 package com.nttung.quiz;
 
 public class Question {
-    private int mTestResId;
+    private int mTextResId;
     private boolean mAnswerTrue;
 
-    public Question(int mTestResId, boolean mAnswerTrue) {
-        this.mTestResId = mTestResId;
+    public Question(int mTextResId, boolean mAnswerTrue) {
+        this.mTextResId = mTextResId;
         this.mAnswerTrue = mAnswerTrue;
     }
 
-    public int getmTestResId() {
-        return mTestResId;
+    public int getmTextResId() {
+        return mTextResId;
+    }
+
+    public void setmTextResId(int mTextResId) {
+        this.mTextResId = mTextResId;
     }
 
     public boolean ismAnswerTrue() {
         return mAnswerTrue;
-    }
-
-    public void setmTestResId(int mTestResId) {
-        this.mTestResId = mTestResId;
     }
 
     public void setmAnswerTrue(boolean mAnswerTrue) {
