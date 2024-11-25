0bbd58c7c697a37fd9b1803af3305efca420841d
tung nguyen <nttung1901@gmail.com>
2024-11-25 15:53:39 +0700

    buoi4

--- CriminalIntent/app/src/main/java/com/nttung/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/com/nttung/criminalintent/Crime.java
@@ -13,4 +13,36 @@ public class Crime {
         mId = UUID.randomUUID();
         mDate = new Date();
     }
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    public void setmId(UUID mId) {
+        this.mId = mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean ismSolved() {
+        return mSolved;
+    }
+
+    public void setmSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
 }
--- CriminalIntent/app/src/main/java/com/nttung/criminalintent/SingleFragmentActivity.java
+++ CriminalIntent/app/src/main/java/com/nttung/criminalintent/SingleFragmentActivity.java
@@ -2,16 +2,12 @@ package com.nttung.criminalintent;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 
-public class MainActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends FragmentActivity {
+    protected abstract Fragment createFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +18,7 @@ public class MainActivity extends FragmentActivity {
         Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
 
         if(fragment == null){
-            fragment = new CrimeFragment();
+            fragment = new CrimeListFragment();
             fragmentManager.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
